5275168f788a49693cfdf6be274658f8331b0897
Yan Vieira <yanrei69@gmail.com>
2023-11-23 10:15:20 -0300

    Novos assets adicionados

---
+++ src/assets/Logo.tsx
@@ -0,0 +1,60 @@
+export default function Logo () {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+        viewBox="0 0 48.019 31.344"
+        xmlSpace="preserve"
+
+        style={{width: '100%', height: '100%'}}
+      >
+        <defs>
+          <linearGradient id="linearGradient9845">
+            <stop offset="0.204" stopColor="#ffda8f" stopOpacity="1"></stop>
+            <stop offset="1" stopColor="#ffa300" stopOpacity="1"></stop>
+          </linearGradient>
+          <linearGradient
+            id="linearGradient9847"
+            x1="52.23"
+            x2="110.844"
+            y1="156.171"
+            y2="171.59"
+            gradientTransform="matrix(.83542 0 0 1 13.326 0)"
+            gradientUnits="userSpaceOnUse"
+            xlinkHref="#linearGradient9845"
+          ></linearGradient>
+        </defs>
+        <g transform="translate(-104.824 -155.448)">
+          <g strokeLinecap="round" transform="translate(26.91 3.126)">
+            <path
+              fill="url(#linearGradient9847)"
+              fillOpacity="1"
+              fillRule="nonzero"
+              stroke="#000"
+              strokeWidth="0"
+              d="M56.96 177.224l13.227-25.647h34.792l-12.91 25.647z"
+              display="inline"
+              transform="translate(20.955 5.383)"
+            ></path>
+            <path
+              fill="#333"
+              strokeLinejoin="round"
+              strokeMiterlimit="4.5"
+              strokeWidth="0.354"
+              d="M113.437 180.668a2.998 2.998 0 01-2.998 2.998 2.998 2.998 0 01-2.998-2.998 2.998 2.998 0 012.998-2.998 2.998 2.998 0 012.998 2.998zm0-12.525a2.998 2.998 0 01-2.998 2.997 2.998 2.998 0 01-2.998-2.997 2.998 2.998 0 012.998-2.998 2.998 2.998 0 012.998 2.998zm5.517 4.311v3.903h-17.03v-3.903z"
+              opacity="1"
+            ></path>
+            <path
+              fill="#333"
+              strokeLinejoin="round"
+              strokeMiterlimit="4.5"
+              strokeWidth="0.435"
+              d="M86.05 163.704v-4.244h18.52v4.244zm7.137-11.383h4.245v18.521h-4.245z"
+              opacity="1"
+            ></path>
+          </g>
+        </g>
+      </svg>
+    )
+  }

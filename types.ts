e6828eda8420661c7b1ccd7f5b359796326cfa42
Yan Vieira <yanrei69@gmail.com>
2023-12-03 17:18:34 -0300

    Ferramenta não necessária removida

--- types.ts
+++ types.ts
@@ -2,12 +2,3 @@ declare interface SvgProps {
     maxWidth?: string
     maxHeight?: string
 }
-
-declare type CalculatorKey = {
-        value: string;
-        type: 'number' | 'operator' | 'complexOperator' | 'function';
-}
-
-declare type KeyHandlers = {
-    [key: string]: (value:string) => void | (() => void);
-}

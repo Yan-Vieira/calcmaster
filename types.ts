a732287aa144cdd69dfc83051f2bc21c26007e3f
Yan Vieira <yanrei69@gmail.com>
2023-12-10 09:37:29 -0300

    Adicionado namespace para os tipos usados na ferramenta Regra de Três Simples

--- types.ts
+++ types.ts
@@ -2,3 +2,16 @@ declare interface SvgProps {
     maxWidth?: string
     maxHeight?: string
 }
+
+declare namespace SROT {
+    type values = {
+        valueA: string,
+        valueB: string,
+        valueC: string,
+        valueD: string
+    }
+
+    type valueXOwner = 'valueA' | 'valueB' | 'valueC' | 'valueD'
+
+    type proportionality = 'directly' | 'indirectly'
+}

48e0140ac83271af998710d0d9d5337de30e3bd3
Yan Vieira <yanrei69@gmail.com>
2023-12-10 09:42:39 -0300

    Correção importante no header

--- src/components/layout/header/app-title/index.tsx
+++ src/components/layout/header/app-title/index.tsx
@@ -2,7 +2,7 @@
 
 import styles from './AppTitle.module.css'
 
-import { useWindowWidth } from '@/hooks/useWindowWidth'
+import useWindowWidth from '@/hooks/useWindowWidth'
 
 import Link from 'next/link'
 import Logo from '@assets/Logo'

df067da26a7f485a04d5d88050392d0a532f1b59
Yan Vieira <yanrei69@gmail.com>
2024-01-10 11:28:59 -0300

    Componentes desnecessários removidos

--- src/components/tools/time-difference/index.ts
+++ src/components/tools/time-difference/index.ts
@@ -1,11 +1,8 @@
-import { Wrapper, Row } from './styledComponents'
 import Input from './input'
 import InputResult from './input-result'
 import SwitchButton from './switch-button'
 
 export default {
-    Wrapper,
-    Row,
     Input,
     InputResult,
     SwitchButton
